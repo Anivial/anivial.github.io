050b149ab1f9e07a69a0f72bb0f1095c7f528fa4
Anivial <emmanuel.tran@partoo.fr>
2022-12-05 23:11:33 +0100

    Test routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import { AppContainer, Header, Logo } from './App.styled';
+import { Routes, Route, Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/about/test">About</Link>
+            <Link to="/careers">Careers</Link>
+        </div>
+    );
+};
 
 
 const App = () => {
     return (
         <AppContainer>
+            <Navbar/>
             <Header>
                 <Logo
                     src={logo}
@@ -14,6 +26,18 @@ const App = () => {
                 <p>
                     Hello World :')
                 </p>
+                <Routes>
+                    <Route path="/" element={<div> / </div>}/>
+                    <Route path="/about/*" element={(
+                        <>
+                            /about
+                            <Routes>
+                                <Route path="/test" element={<div> /test </div>}/>
+                            </Routes>
+                        </>
+                    )}/>
+                    <Route path="/careers" element={<div> /careers </div>}/>
+                </Routes>
             </Header>
         </AppContainer>
     );
